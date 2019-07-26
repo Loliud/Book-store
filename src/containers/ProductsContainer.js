8b1e07004bed9ae99dba7d1677a07c01e3358037
Loliud <anhhaiquynhon123@gmail.com>
2019-07-26 21:46:08 +0700

    demo axios

--- src/containers/ProductsContainer.js
+++ src/containers/ProductsContainer.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import Products from '../components/ForProductsPage/Products';
 import {connect} from 'react-redux';
+import axios from 'axios';
 class ProductsContainer extends Component {
+
+    constructor(props){
+        super(props);
+        this.state ={
+            products: []
+        }
+    }
+
+    componentDidMount(){
+        axios.get('https://5d3029eb28465b00146aaca5.mockapi.io/api/products')
+        .then((res) =>{
+            console.log(res.data);
+            this.setState({
+                products: res.data
+            });
+        });
+    }
     
     render() {
-
-        let {products} = this.props;
+       
+       
         return (
            <Products>
                {   // chuyen cho children la 1 object
                    {
-                       products: products
+                       products: this.state.products
                    }
                }
            </Products>
